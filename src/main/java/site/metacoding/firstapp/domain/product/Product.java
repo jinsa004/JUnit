4811b6dcbb309185f0a8a2a96da4072e925d2767
jinsa <jinsa004@gmail.com>
2022-09-30 11:19:00 +0900

    ProductDao Junit 테스트 완료

--- src/main/java/site/metacoding/firstapp/domain/product/Product.java
+++ src/main/java/site/metacoding/firstapp/domain/product/Product.java
@@ -3,14 +3,32 @@ package site.metacoding.firstapp.domain.product;
 import java.sql.Timestamp;
 
 import lombok.Getter;
-import lombok.Setter;
 
-@Setter
 @Getter
 public class Product {
     private Integer productId;
     private String productName;
-    private String productPrice;
+    private Integer productPrice;
     private Integer productQty;
     private Timestamp createdAt;
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    private Product() {
+    } // MyBatis에게 필요한 것.
+
+    public Product(String productName, Integer productPrice, Integer productQty) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productQty = productQty;
+    }
+
+    public void update(Product product) {
+        this.productName = product.getProductName();
+        this.productPrice = product.getProductPrice();
+        this.productQty = product.getProductQty();
+    }
+
 }
